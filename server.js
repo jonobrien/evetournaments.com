d33136e7a88979638df2bb23c35126e842e080c4
Jon O'Brien <jvo7822@rit.edu>
2016-08-03 16:59:54 +0000

    added client caching, for now - prototyping

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ var path = require('path');
 var portno = 3000;
 
 // viewed at http://localhost:portno
-app.use('/static', express.static('static'));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/tournaments.html'));
-});
+if (process.env.EVE_JS_PROD) {
+    console.log("prod setup");
+    app.use('/eve/static', express.static('static'));
+    app.get('/eve/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/tournaments.html'));
+    });
+}
+else {
+    console.log("dev setup");
+    app.use('/static', express.static('static'));
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/tournaments.html'));
+    });
+}
 
 app.listen(portno);
 console.log("running on " + portno)
--- static/js/tournaments.js
+++ static/js/tournaments.js
@@ -1,26 +1,62 @@
+/*
+ * Proof of concept CREST parsing for tournaments endpoints
+ * series data and team data parsed
+*/
 
 
 $(document).ready(function() {
-
-
+  populateTournaments();
+  $('ui,dropdown').dropdown();
   $('#ft').append("data pulled from <a target='blank' href='https://crest-tq.eveonline.com/tournaments/1/series/'>here</a>");
 
 });// doc.ready
-function getSeries() {
-    var url = "https://crest-tq.eveonline.com/tournaments/1/series/";
-    queryCrestSeries(url);
+
+
+function populateTournaments() {
+
 }
 
-function queryCrestSeries(crestURL) {
-    $.ajax({
+
+/*
+ * assuming we pass the url to the new page
+ * we can render the data after a caching attempt
+ * yes client-side caching is horribly insecure
+ * but needed proof of concept first
+*/
+function getSeries() {
+    $('#series').empty();
+    var url = "https://crest-tq.eveonline.com/tournaments/1/series/";
+    var additional = "/tournaments/1/series/";
+    // check if data is already cached
+    var cachedData = getCached(additional);
+    // expired or not cached, get new data
+    if (cachedData === null) {
+        console.log("getting new data");
+        cachedData = queryCrest(url);
+        cachedData.success(function(resp) {
+            console.log('success');
+            console.log(resp);
+            cache(resp, additional);
+            parseSeriesData(resp);
+        });
+    }
+    else {
+        parseSeriesData(cachedData[additional]);
+    }
+}
+
+
+/*
+ * not cached or expired cache, get data
+*/
+function queryCrest(crestURL) {
+    console.log('querying for new data at: \n');
+    console.log(crestURL);
+    return $.ajax({
       type: "GET",
       url: crestURL,
-      success: function(data) {
-        $('#series').empty();
-        parseSeriesData(data);
-      }
     });
-  }
+}
 
 
 /*
@@ -28,7 +64,7 @@ function queryCrestSeries(crestURL) {
  * heading: Bye | Winner | Red FC | Blue FC | Series Matches Won
 */
 function parseSeriesData(data) {
-  var lenItems = data['items'].length;
+  var lenItems = data['totalCount'];
   var i = 0;
   var redT = '';
   var blueT = '';
@@ -39,14 +75,14 @@ function parseSeriesData(data) {
     if ('isBye' in data['items'][i]['redTeam']
     				&& data['items'][i]['redTeam']['isBye'] === true) {
       blueT = winner = data['items'][i]['winner']['team']['teamName'];
-      redT = 'N/A - Bye'
+      redT = 'N/A - Bye';
       $('#series').append('<td class="ui info message"><i class="icon circle"></i></td>');
     }
     // redTeam has bye, no blueTeam present, red wins
     else if ('isBye' in data['items'][i]['blueTeam']
     				&& data['items'][i]['blueTeam']['isBye'] === true) {
       redT = winner = data['items'][i]['winner']['team']['teamName'];
-      blueT = 'N/A - Bye'
+      blueT = 'N/A - Bye';
       $('#series').append('<td class="negative"><i class="icon circle"></i></td>');
     }
     // nobody wins by default, get both teams and winner
@@ -76,19 +112,58 @@ function parseSeriesData(data) {
 }
 
 
+/*
+ * determine if browser supports localStorage
+ * then determine if the data is already cached
+ * returns data if cached or null if not cached yet
+ * returns null if expired
+*/
+function getCached(queryStr) {
+    var data = null;
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.getItem(queryStr) !== null) {
+            data = JSON.parse(localStorage.getItem(queryStr));
+            // if data hasn't expired yet return it
+            var now = new Date().getTime();
+            if (now < data.cached_until) {
+                console.log('valid cached until: ' + new Date(data.cached_until));
+                console.log(data);
+                return data;
+            }
+            // otherwise don't return it
+            else {
+                console.log('cached data expired, return null');
+                console.log('cache_expires: ' + data.cached_until);
+                console.log('          now: ' + now);
+                console.log('         diff: ' + (now-data.cached_until));
+                data = null;
+            }
+        }
+        // data is null anyway don't need else {}
+    }
+    else {
+        console.log("localStorage unsupported");
+    }
+    return data;
+}
 
-function getSeriesTeam(crestURL) {
-    $.ajax({
-      type: "GET",
-      url: crestURL,
-      success: function(data) {
-        $('#series').empty();
-        parseTeamData(data);
-      }
-    });
-  }
 
-
-  function parseTeamData(data) {
-
-  }
+/*
+ * data is cached for 5 minutes if no parameter passed
+ * actually need to parse cache-control header for offset
+*/
+function cache(data, queryStr, cacheOffset) {
+    var offset = cacheOffset
+    var cacheDuration = new Date().getTime(); // now
+    if (offset === undefined) {
+        offset = 300000;
+    }
+    cacheDuration = cacheDuration + offset;
+    var cacheMe = {};
+    cacheMe[queryStr] = data;
+    // cache for 5 minutes (30000 ms)
+    cacheMe['cached_until'] = cacheDuration;
+    // stringify the object so getItem returns the object
+    localStorage.setItem(queryStr, JSON.stringify(cacheMe));
+    console.log("cached until: " + cacheMe.cached_until);
+}
