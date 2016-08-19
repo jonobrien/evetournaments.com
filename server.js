f4ac234796837b9a98b8ce1b63cc64a3622ec168
Jon O'Brien <jvo7822@rit.edu>
2016-08-18 20:02:55 -0400

    begin backend https queries

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var portno = 3000;
+const express = require('express');
+const app = express();
+const path = require('path');
+const portno = 3000;
+
+
+
+
+
+/*
+const https = require('https');
+
+var options = {
+  hostname: 'crest-tq.eveonline.com',
+  port: 443,
+  path: '/',
+  method: 'GET'
+};
+
+var req = https.request(options, function(res) {
+  console.log('statusCode:', res.statusCode);
+  console.log();
+  console.log('headers:', res.headers);
+
+  res.on('data', function(d) {
+    process.stdout.write(d);
+    console.log();
+  });
+});
+req.end();
+
+req.on('error', function(e) {
+  console.error('failed');
+  console.error(e);
+});
+*/
+
+
+////////////////////////////////////////////////////////
+
 
 // viewed at http://localhost:portno
 if (process.env.EVE_JS_PROD) {
@@ -20,4 +56,4 @@ else {
 }
 
 app.listen(portno);
-console.log("running on " + portno)
+console.log("running on " + portno);
